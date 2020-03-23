2de8c10f91d560494b6784b62da0cf21d1e848be
Tobi Tselios <tobi.tselios@gmail.com>
2020-03-23 17:22:30 +0000

    Added a MockDataLoader to the Testing Target

---
+++ SearrchResultsControllerTests/MockDataLoading.swift
@@ -0,0 +1,28 @@
+//
+//  MockDataLoading.swift
+//  SearrchResultsControllerTests
+//
+//  Created by Tobi Kuyoro on 23/03/2020.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+@testable import iTunes_Search
+
+class MockDataLoader: NetworkDataLoader {
+   let data: Data?
+   let resposne: URLResponse?
+   let error: Error?
+   
+   internal init(data: Data?, resposne: URLResponse?, error: Error?) {
+      self.data = data
+      self.resposne = resposne
+      self.error = error
+   }
+   
+   func loadData(using request: URLRequest, completion: @escaping (Data?, URLResponse?, Error?) -> Void) {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 0.005) {
+         completion(self.data, self.resposne, self.error)
+      }
+   }
+}
