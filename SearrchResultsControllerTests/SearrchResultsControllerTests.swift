70a9676ddfd1289794612645979ffa293304478e
Tobi Tselios <tobi.tselios@gmail.com>
2020-03-23 16:32:13 +0000

    Added a Test with an Expectation

--- SearrchResultsControllerTests/SearrchResultsControllerTests.swift
+++ SearrchResultsControllerTests/SearrchResultsControllerTests.swift
@@ -7,27 +7,32 @@
 //
 
 import XCTest
+@testable import iTunes_Search
+
+/*
+ 
+ - Does decoding work?
+ - Does decoding fail when given bad data?
+ - Does it build the correct URL?
+ - Does it build the correct URLRequest?
+ - Are the search results saved properly?
+ - Is the completion handler called when data is good?
+ - Is the completion handler called when data is bad?
+ - Is the completion handler called when networking fails?
+ 
+ */
 
 class SearrchResultsControllerTests: XCTestCase {
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+   func testForSomeResults() {
+      let controller = SearchResultController()
+      let expectation = self.expectation(description: "Wait for results")
+      
+      controller.performSearch(for: "GarageBand", resultType: .software) {
+//         XCTFail()
+         expectation.fulfill()
+      }
+      
+      wait(for: [expectation], timeout: 5)
+   }
 }
